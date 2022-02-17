5d4ec11f4389a328b55ce2c4998bb03add171645
Jonathan Rein <jrein1296@gmail.com>
2022-02-17 14:46:10 -0500

    Updating redis.helper.js const client to user string rather than process.env.

--- server/src/helpers/redis.helper.js
+++ server/src/helpers/redis.helper.js
@@ -1,5 +1,5 @@
 const redis = require("redis");
-const client = redis.createClient(process.env.REDIS_URL);
+const client = redis.createClient("redis://localhost:6379");
 
 client.on("connect", () => {
   console.log("Redis client is connected");

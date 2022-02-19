b4ebbd2523b25e248fc286ea3cc010bdd9a13f2e
Jonathan Rein <jrein1296@gmail.com>
2022-02-19 12:59:16 -0500

    Updates to notes.

--- server/src/helpers/redis.helper.js
+++ server/src/helpers/redis.helper.js
@@ -1,5 +1,6 @@
 const redis = require("redis");
-const client = redis.createClient(process.env.REDIS_URL);
+// const client = redis.createClient(process.env.REDIS_URL);
+const client = redis.createClient("redis://localhost:6379");
 
 
 client.on("connect", () => {

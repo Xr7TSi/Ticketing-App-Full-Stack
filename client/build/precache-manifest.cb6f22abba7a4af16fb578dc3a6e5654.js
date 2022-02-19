3588674d692ac3b8972c63a74e1a256bd1fb95ea
Jonathan Rein <jrein1296@gmail.com>
2022-02-18 22:10:34 -0500

    Additions to notes.  Removing notes from gitignore.

--- client/build/precache-manifest.cb6f22abba7a4af16fb578dc3a6e5654.js
+++ client/build/precache-manifest.cb6f22abba7a4af16fb578dc3a6e5654.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fa1596b66ba070b40ed8b4ffe0db70b2",
+    "revision": "03432585740114870596ecf296ca278c",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.68a81a89.chunk.css"
   },
   {
-    "revision": "9e4388da5e75a7a28e3c",
-    "url": "/static/css/main.ee7dff04.chunk.css"
+    "revision": "08526b4bfd34eb509ad6",
+    "url": "/static/css/main.7e795059.chunk.css"
   },
   {
     "revision": "5b9ec1a3f6d00bf7b086",
     "url": "/static/js/2.2c8851fe.chunk.js"
   },
   {
-    "revision": "9e4388da5e75a7a28e3c",
-    "url": "/static/js/main.4e3be779.chunk.js"
+    "revision": "08526b4bfd34eb509ad6",
+    "url": "/static/js/main.af6a3189.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.87d6ca396bc2c611d449a78fc4b3f123.js"
+  "/precache-manifest.cb6f22abba7a4af16fb578dc3a6e5654.js"
 );
 
 self.addEventListener('message', (event) => {
